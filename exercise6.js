f627cc06cb51f2f322779395e62afaeef13ad251
Sirui Yao <20095250@mail.wit.ie>
2023-09-17 21:47:38 +0100

    Exercise 6.

--- exercise6.js
+++ exercise6.js
@@ -1,11 +1,18 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(json => {
-     const completed = null // Complete this code
-     completed.forEach( (todo, index) => {
-      console.log(`${todo.title} - ${todo.completed}`)
-    })
+    const completedTodosPerUser = json.reduce((result, todo) => {
+      if (todo.completed) {
+        if (result[todo.userId]) {
+          result[todo.userId]++;
+        } else {
+          result[todo.userId] = 1;
+        }
+      }
+      return result;
+    }, {});
+    console.log(completedTodosPerUser);
   })
-  .catch(function(err) { 
+  .catch(function(err) {
     console.log(err);
-  })
+  });
