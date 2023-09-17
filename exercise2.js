dda5a536b7163e7a015d0902dcf18ce2ea183360
Sirui Yao <20095250@mail.wit.ie>
2023-09-17 21:42:48 +0100

    Exercise 2.

--- exercise2.js
+++ exercise2.js
@@ -1,12 +1,9 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(json => {
-     const completed = null // Complete this code
-     completed.forEach( (todo, index) => {
-      console.log(`${todo.title} - ${todo.completed}`)
-    })
+    const todoTitles = json.map(todo => todo.title);
+    console.log(todoTitles);
   })
-  .catch(function(err) { 
+  .catch(function(err) {
     console.log(err);
-  })
-  console.log(todoTitles)
+  });
